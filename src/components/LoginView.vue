29c092a4cd64685c910db70a11103713819b672a
纵昂 <zongang91@gmail.com>
2022-12-28 10:46:02 +0800

    美化登陆页面

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,8 +1,7 @@
 <template>
-  <div>
-    <el-card class="box-card">
-        <h1>XXX管理系统</h1>
-    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+  <div class="login">
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="login-form">
+        <h3 class="title">真爱后台管理系统</h3>
         <el-form-item label="账号" prop="pass">
             <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
         </el-form-item>
@@ -13,7 +12,7 @@
             <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
         </el-form-item>
     </el-form>
-    </el-card>
+
   </div>
 </template>
 
@@ -71,11 +70,34 @@ export default {
 </script>
 
 <style>
+.login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-image: url("../assets/login-background.jpg");
+    background-size: cover;
+}
+
+.login-form{
+    border-radius: 6px;
+    background: #ffffff;
+    width: 400px;
+    padding: 25px 25px 5px 25px;
+    
+}
+.el-input {
+    height: 38px;
+}
+.input-icon {
+    height: 39px;
+    width: 14px;
+    margin-left: 2px;
+}
 .box-card {
     margin-top: 120px;
     margin-left: 420px;
     width: 480px;
     height: 300px;
 }
-
 </style>
